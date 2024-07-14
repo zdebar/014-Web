712b86b19b00d491d0489bcd63b5679311c79483
zdebar <157598603+zdebar@users.noreply.github.com>
2024-07-14 17:17:34 +0200

    02-Calculator

---
+++ 02-Calculator/javascript.js
@@ -0,0 +1,53 @@
+let number1;
+let number2;
+let operator;
+let displayValue;
+
+
+
+function add(a, b) {
+  return a + b
+}
+
+function substract(a, b) {
+  return a - b
+}
+
+function multiply(a, b) {
+  return a * b
+}
+
+function divide(a, b) {
+  return a / b
+}
+
+function operate(a, operator, b) {
+  return operator(a, b)
+}
+
+
+
+function handleNumberButtons (button) {
+  button.addEventListener("click",()=>{ 
+    displayValue = button.innerText
+
+  });
+}
+
+function printToDisplay (number) {
+  document.getElementById("display").innerText = number.toString();
+}
+
+function selectAllButtons () {
+  document.querySelectorAll("button").forEach(button => {
+    handleNumberButtons(button);
+  });
+}
+
+function assignSymbols () {
+  
+}
+
+
+// Assign Event listeners to all buttons
+selectAllButtons();

3dd9388b61b340adae992e635e83900f473ce2fb
zdebar <157598603+zdebar@users.noreply.github.com>
2024-08-01 20:01:25 +0200

    installing Express

---
+++ express_app.js
@@ -0,0 +1,5 @@
+// Every thrown error in the application or the previous middleware function calling `next` with an error as an argument will eventually go to this middleware function
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send(err);
+});
